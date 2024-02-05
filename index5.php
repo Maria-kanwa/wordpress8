7f9d939ea8fc9ef6f7794279f0978e3bb6a0617a
Maria kanwal <you@example.com>
2024-02-05 16:09:10 +0500

    first commit

---
+++ index5.php
@@ -0,0 +1,26 @@
+<?php
+$num=9;
+switch ($num)
+{
+  case 1:
+    echo "one";
+    break;
+     case 2:
+    echo "two";
+    break;
+     case 3:
+    echo "three";
+    break;
+     case 4:
+    echo "four";
+    break;
+     case 5:
+    echo "five";
+    break;
+     case 6:
+    echo "six";
+    break;
+    default;
+    echo "number not found";
+}
+?>
